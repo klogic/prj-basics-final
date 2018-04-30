c229865ac5327c9eb611b8c21467b29ebbbe97d9
Narongsak Keawmanee <klogic@hotmail.co.th>
2018-04-30 08:37:15 +0700

    split single page by two section & navigate by function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loadedHeader = 'recipe'
+  onNavigateclick(data:string){
+    this.loadedHeader = data
+  }
 }
